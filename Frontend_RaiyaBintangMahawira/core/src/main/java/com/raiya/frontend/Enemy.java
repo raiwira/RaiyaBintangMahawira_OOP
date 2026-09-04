6ced88e4f5cbf4d7b91a380137ed6b2df089d554
raiyay <r.bintang0109@gmail.com>
2026-09-04 10:42:17 +0700

    CS_MN_RaiyaBintangMahawira_2506554991_OOP1

--- Frontend_RaiyaBintangMahawira/core/src/main/java/com/raiya/frontend/Enemy.java
+++ Frontend_RaiyaBintangMahawira/core/src/main/java/com/raiya/frontend/Enemy.java
@@ -1,4 +1,47 @@
 package com.raiya.frontend;
 
 public class Enemy {
+
+    // Problem 3 — Creating Enemy Attributes
+    public String name;
+    public int hp;
+    public int maxHp;
+
+    // Problem 4 — Creating the Enemy Constructor
+    public Enemy(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+
+        // Setting maxHp to match the initial starting hp, as required
+        this.maxHp = hp;
+    }
+
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        this.hp -= damage;
+        // 2. HP must not go below 0.
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        // 3. Display the current HP in the format: [EnemyName] took [damage] damage! HP: [currentHP]/[maxHP]
+        if (this.hp > 0)
+            System.out.println(this.name + "took " + damage + "damage!!, HP : [" + this.hp + this.maxHp + "]");
+
+            // 4. If HP reaches 0, display that the Enemy has been defeated, in the format: [EnemyName] was defeated!
+        else {
+            System.out.println(this.name + " was defeated!!!!");
+        }
+    }
+    public void attack(Player player, int damage) {
+        // 1. Display information that the Enemy is attacking the Player, in the format: [EnemyName] unleashes bullet barrage on [PlayerName]!
+        System.out.println(this.name + " unleashes buller barrage on " + player.name +  "!");
+        // 2. Call the Player's takeDamage() method using the given damage.
+        player.takeDamage(damage);
+    }
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        return this.hp > 0;
+    }
+
+
 }
--- Frontend_RaiyaBintangMahawira/core/src/main/java/com/raiya/frontend/Player.java
+++ Frontend_RaiyaBintangMahawira/core/src/main/java/com/raiya/frontend/Player.java
@@ -1,4 +1,47 @@
 package com.raiya.frontend;
 
 public class Player {
+    public String name;
+    public int hp;
+    public int power;
+    public int spellCards;
+
+    public Player(String name, int hp, int power, int spellCards) {
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.spellCards = spellCards;
+    }
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        this.hp -= damage;
+
+        // 2. HP must not become negative.
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        // 3. If HP is still greater than 0, display the remaining HP in the format: [PlayerName] took [damage] damage! Remaining HP: [hp]
+        if (this.hp > 0)
+            System.out.println(this.name + "took " + damage + "damage, Remaining HP : " + this.hp);
+            // 4. If HP reaches 0, display a message that the Player has been defeated.
+        else {
+            System.out.println(this.name + " has been defeated");
+        }
+    }
+    public void shoot(Enemy target) {
+        // 1. Create an int named damage, calculated by adding 10 to power.
+        int damage = this.power + 10;
+        // 2. Display information that the Player is shooting the Enemy, in the format: [name] shoots [TargetName] dealing [damage] DMG!
+        System.out.println(this.name + " shoots " + target.name + " dealing " + damage + " DMG!!");
+        // 3. Call the Enemy object's takeDamage() method.
+        target.takeDamage(damage);
+    }
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        return this.hp > 0;
+    }
+
+
+
+
 }
